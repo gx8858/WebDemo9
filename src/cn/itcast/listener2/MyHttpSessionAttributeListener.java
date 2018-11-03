4ed0e34acc5f690cf4750b75e602185cc46563ec
GaoXiang <39550837+gx8858@users.noreply.github.com>
2018-11-03 14:54:30 +0800

    域对象属性的状态改变的监听器HttpSessionAttributeListener

--- src/cn/itcast/listener2/MyHttpSessionAttributeListener.java
+++ src/cn/itcast/listener2/MyHttpSessionAttributeListener.java
@@ -3,6 +3,9 @@ package cn.itcast.listener2;
 import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionBindingEvent;
 
+/**
+ * 域对象属性的状态改变的监听器HttpSessionAttributeListener
+ */
 public class MyHttpSessionAttributeListener implements HttpSessionAttributeListener{
 
 	@Override
