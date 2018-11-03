3bc4b979043f5f71fe28b6f239cba4a9e055505a
GaoXiang <39550837+gx8858@users.noreply.github.com>
2018-11-03 14:38:56 +0800

    域对象的创建和销毁的监听器ServletContextListener

--- src/cn/itcast/listener1/MyHttpSessionListener.java
+++ src/cn/itcast/listener1/MyHttpSessionListener.java
@@ -3,6 +3,9 @@ package cn.itcast.listener1;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+/**
+ * 域对象的创建和销毁的监听器ServletContextListener
+ */
 public class MyHttpSessionListener implements HttpSessionListener{
 
 	@Override
