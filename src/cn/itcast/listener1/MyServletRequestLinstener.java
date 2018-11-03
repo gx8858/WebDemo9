598e8bf0d5ed181a9c0cfe5337eadeeaeb9504b7
GaoXiang <39550837+gx8858@users.noreply.github.com>
2018-11-03 14:43:54 +0800

    域对象的创建和销毁的监听器ServletRequestListener

--- src/cn/itcast/listener1/MyServletRequestLinstener.java
+++ src/cn/itcast/listener1/MyServletRequestLinstener.java
@@ -3,6 +3,9 @@ package cn.itcast.listener1;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 
+/**
+ * 域对象的创建和销毁的监听器ServletRequestListener
+ */
 public class MyServletRequestLinstener implements ServletRequestListener{
 	
 	public void requestInitialized(ServletRequestEvent sre) {
